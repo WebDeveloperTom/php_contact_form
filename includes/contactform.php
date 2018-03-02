8b30cbb3884cd07931ec040802146c127dd7c815
WebDeveloperTom <tompowellgames@gmail.com>
2018-03-02 22:04:41 +1300

    add error messages

---
+++ includes/contactform.php
@@ -0,0 +1,58 @@
+<?php
+$name = $phone = $subject = $mailFrom = $message = "";
+
+if (isset($_POST['submit'])) {
+  include_once 'dbh.inc.php';
+  $name = $_POST['name'];
+  $phone = $_POST['phone'];
+  $subject = $_POST['subject'];
+  $mailFrom = $_POST['mail'];
+  $message = htmlspecialchars($_POST['message']); //ensures scripts cant be typed into "messages"
+
+    //error handling
+    //check if inputs are empty
+    if (empty($name) || empty($phone) || empty($subject) || empty($mailFrom) || empty($message)){
+      header("Location: ../index.php?query=empty&name=$name&phone=$phone&subject=$subject&message=$message");
+      exit();
+    } else {
+      //Check if name is valid
+      if(!preg_match("/^[a-zA-Z\s]*$/", $name)){
+        header("Location: ../index.php?query=invalidchar");
+        exit();
+      } else {
+        //check if phone numbe is a number
+          if(!is_numeric($phone)){
+            header("Location: ../index.php?query=invalidemail&name=$name&subject=$subject&message=$message");
+            exit();
+        } else {
+          //check if email is an email
+          if (!filter_var($mailFrom, FILTER_VALIDATE_EMAIL)){
+            header("Location: ../index.php?query=invalidemail&name=$name&phone=$phone&subject=$subject&message=$message");
+            exit();
+          } else {
+            if(!preg_match("/^[a-zA-Z\s]*$/", $subject)){
+              header("Location: ../index.php?query=invalidchar");
+              exit();
+          } else {
+            //success, no errors
+            $mailTo = "tom@webdevelopertom.com";  //clients email
+            $headers = "From: ". $mailFrom;
+            $txt = "You have recieved an email from ". $name." (".$phone.")".". \n \n".$message;
+
+
+            mail($mailTo, $subject, $txt, $headers);
+            header("Location: ../index.php?query=sent");
+
+            $sql = "INSERT INTO form_data (Name, Email, Phone, Message) VALUES ('$name', '$mailFrom', '$phone', '$message');";
+            mysqli_query($conn, $sql);
+          }
+        }
+      }
+    }
+  }
+} else {
+  header("Location: ../index.php?query=error");
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+
+  </head>
+  <body>
+
+    <main>
+      <div class="container">
+        <p>Contact Us</p>
+        <form class="contact-form" action="includes/contactform.php" method="post">
+          <!-- returns user input if a form error occurs -->
+          <?php
+            if (isset($_GET['name'])) {
+              $name = $_GET['name'];
+              echo '<input type="text" name="name" placeholder="Full Name" value="'.$name.'">';
+            } else {
+              echo '<input type="text" name="name" placeholder="Full Name">';
+            }
+            if (isset($_GET['phone'])) {
+              $phone = $_GET['phone'];
+              echo '<input type="text" name="phone" placeholder="Phone Number" value="'.$phone.'">';
+            } else {
+              echo '<input type="text" name="phone" placeholder="Phone Number">';
+            }
+           ?>
+          <input type="text" name="mail" placeholder="Your e-mail">
+          <?php
+          if (isset($_GET['subject'])) {
+            $subject = $_GET['subject'];
+            echo '<input type="text" name="subject" placeholder="Subject" value="'.$subject.'">';
+          } else {
+            echo '<input type="text" name="subject" placeholder="Subject">';
+          }
+          if (isset($_GET['message'])) {
+            $message = $_GET['message'];
+            echo '<textarea name="message" placeholder="Message" rows="8" cols="80">'.$message.'</textarea>';
+          } else {
+            echo '<textarea name="message" placeholder="Message" rows="8" cols="80"></textarea>';
+          }
+           ?>
+          <button type="submit" name="submit">Send</button>
+        </form>
+        
+        <!-- error messages -->
+        <?php
+          if (!isset($_GET['query'])) {
+            exit();
+          }
+          else {
+            $queryCheck = $_GET['query'];
+            if ($queryCheck == "empty" ) {
+              echo "<p class='error'>You did not fill out all the fields</p>";
+              exit();
+            } elseif ($queryCheck == "invalidchar" ) {
+              echo "<p class='error'>Please use A - Z characters only in Name and Subject.</p>";
+              exit();
+            } elseif ($queryCheck == "invalidemail" ) {
+              echo "<p class='error'>You did not enter a valid email.</p>";
+              exit();
+            } elseif ($queryCheck == "sent" ) {
+              echo "<p class='success'>Your message has been received! Someone will be in touch soon.</p>";
+
+            }
+          }
+
+         ?>
+      </div>
+
+    </main>
+  </body>
+</html>
